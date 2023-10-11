a7536001ac5692267b3feeef249e0abca1123dbb
philcifone <phil@philcifone.com>
2023-10-11 02:04:17 -0400

    work on front end, set routes, create scenes

--- server/index.js
+++ server/index.js
@@ -64,7 +64,7 @@ mongoose.connect(process.env.MONGO_URL, {
  .then(() => {
   app.listen(PORT, () => console.log(`Server Port: ${PORT}`));
 
-   /* ADD DATA ONE TIME */
+   /* ADD TEST DATA ONE TIME */
    // User.insertMany(users);
    // Post.insertMany(posts);
  })
